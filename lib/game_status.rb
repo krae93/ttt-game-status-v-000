8ba314c3704eef4c34c0e6fb983abb636b7c0280
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-07-18 16:00:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,7 +14,7 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [2,4,6]  
+  [2,4,6]
 ]
 
 
@@ -23,51 +23,44 @@ def won?(board)
   win_index_1 = win_move[0]
   win_index_2 = win_move[1]
   win_index_3 = win_move[2]
- 
-  position_1 = board[win_index_1] 
+
+  position_1 = board[win_index_1]
   position_2 = board[win_index_2]
-  position_3 = board[win_index_3] 
- 
+  position_3 = board[win_index_3]
+
   if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
     return win_move
-  end 
   end
-else 
-  false 
-end 
+  end
+else
+  false
+end
 
   def full?(board)
     !board.any? { |x| x == " " }
   end
 
-  
+
   def draw?(board)
     if  !won?(board) && full?(board)
     puts "the game has been won"
-      return true 
-    else 
+      return true
+    else
       puts "the game has not been won"
-  return false 
-    end 
-  end 
-  # def draw?(board)
-  #   full?(board) && !won?(board)
-  # end
-   
+  return false
+    end
+  end
+  
 def over?(board)
 if  full?(board) || won?(board) || draw?(board)
   puts "THE GAME IS OVER"
-  return true  
-else 
-  puts "the game is not over" 
-  return false 
-end 
-end 
+  return true
+else
+  puts "the game is not over"
+  return false
+end
+end
 
-  # def over?(board)
-  #   full?(board) || won?(board)
-  # end
- 
   def winner(board)
     if winner = won?(board)
       board[winner.first]
